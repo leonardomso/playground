6ed16618d90db8dd8ab1bc4eab46e91b8c6dc79d
leonardomso <leonardomso11@gmail.com>
2019-06-25 15:02:04 -0300

    Removing server

--- packages/server/src/graphql/typeDefs.ts
+++
@@ -1,9 +0,0 @@
-import { mergeTypes } from "merge-graphql-schemas";
-
-import User from "./User/typeDefs";
-import Auth from "./Auth/typeDefs";
-import Error from "./Error/typeDefs";
-
-const typeDefs = [User, Auth, Error];
-
-export default mergeTypes(typeDefs);
